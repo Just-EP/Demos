5e919142bc2da475e165b8b92e1ef9dbe0de700a
Just-EP <lovelive7185@outlook.com>
2019-07-14 16:33:18 +0800

    没什么特别的先提交再说

--- httpclient/src/main/java/pers/crawler/httpclient/HttpClientCommon.java
+++ httpclient/src/main/java/pers/crawler/httpclient/HttpClientCommon.java
@@ -1,43 +1,46 @@
 package pers.crawler.httpclient;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.junit.jupiter.api.Test;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 
 /**
  * @author JustEP
  * @version 1.0
- * @classname HttpClientDemo
- * @description 测试httpClient，试着使用httpclient和jsoup获取B站排行榜信息
- * @createtime 2019/07/08 22:14:00
+ * @classname HttpClientCommon
+ * @description 通用
+ * @createtime 2019/07/14 15:19:00
  */
-public class HttpClientDemo {
-    private static CloseableHttpClient httpClient = HttpClients.createDefault();
-    @Test
-    public void getBiliBiliRanking() {
-        //b站排行榜url https://www.bilibili.com/ranking/
-        //all全站榜，1动画区，1近期投稿，3三日排行
+public class HttpClientCommon {
+    private static BasicCookieStore cookieStore = new BasicCookieStore();
+    private static CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+    public static void main(String[] args) {
+        new HttpClientCommon().getHtml("https://music.163.com/#/playlist?id=106464264");
+    }
+    public void getHtml(String url){
         try {
-            String url = "https://www.bilibili.com/ranking/all/1/1/3/";
+//            cookieStore.addCookie(new BasicClientCookie());
             HttpGet get = new HttpGet(url);
             get.setHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
+            Header[] allHeaders = get.getAllHeaders();
+            System.out.println(get);
             CloseableHttpResponse response = httpClient.execute(get);
             HttpEntity entity = response.getEntity();
             String html = new String(EntityUtils.toString(entity,"UTF-8").getBytes(), "UTF-8");
             Document document = Jsoup.parse(html);
-            System.out.println(document);
-//            Elements elements = document.getElementsByClass("rank-item");
-//            for (Element element:elements){
-//                System.out.println(element);
-//            }
+            Element body = document.body();
+//            System.out.println(body);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -47,5 +50,6 @@ public class HttpClientDemo {
                 e.printStackTrace();
             }
         }
+
     }
 }
