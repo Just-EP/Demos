2c97965c774ff05a94c3cb894c3c69f7f5491d7e
Just-EP <lovelive7185@outlook.com>
2020-05-02 10:11:13 +0800

    Bot

---
+++ ffmpeg4springboot/src/main/java/pers/demos/ffmpeg4springboot/Ffmpeg4springbootApplication.java
@@ -0,0 +1,13 @@
+package pers.demos.ffmpeg4springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ffmpeg4springbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ffmpeg4springbootApplication.class, args);
+    }
+
+}
---
+++ ffmpeg4springboot/src/test/java/pers/demos/ffmpeg4springboot/Ffmpeg4springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.demos.ffmpeg4springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ffmpeg4springbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
